0d317f0b4fffb0136ccea97e71de3e519b1f9768
Stefano Brembati <stefano.brembati@scao.it>
2022-09-26 17:27:15 +0200

    Completed module

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', (req,res) => res.render('index', {title: 'Index'}))
+
+app.listen(PORT, () => console.log('STA A RUNNA SULLA PORTA',PORT))
