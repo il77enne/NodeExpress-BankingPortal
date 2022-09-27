af740fc064137e3222b52d63ec0f4e7efb4a2d9f
Stefano Brembati <stefano.brembati@scao.it>
2022-09-27 09:23:10 +0200

    Completed module

--- src/app.js
+++ src/app.js
@@ -11,6 +11,23 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', (req,res) => res.render('index', {title: 'Index'}))
+
+const accountData = fs.readFileSync('.\\src\\json\\accounts.json',{encoding: 'utf8'})
+
+const accounts = JSON.parse(accountData)
+
+
+const userData  = fs.readFileSync('.\\src\\json\\users.json',{encoding: 'utf8'})
+
+const user = JSON.parse(userData)
+
+
+app.get('/savings', (req,res) => res.render('account', {title: 'Account Summary', account: accounts.savings}))
+app.get('/checking', (req,res) => res.render('account', {title: 'Account Summary', account: accounts.checking }))
+app.get('/credit ', (req,res) => res.render('account', {title: 'Account Summary', account: accounts.credit }))
+
+app.get('/profile', (req,res) => res.render('profile', {title: 'Account Summary', user: user[0]}))
+
+app.get('/', (req,res) => res.render('index', {title: 'Account Summary', accounts: accounts}))
 
 app.listen(PORT, () => console.log('STA A RUNNA SULLA PORTA',PORT))
